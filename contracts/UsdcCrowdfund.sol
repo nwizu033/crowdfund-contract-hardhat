ca371e13eb4f97704d4f00dbf46111e9bd927958
chinwizu <chinwianthony@gmail.com>
2023-01-28 22:11:24 +0100

    test continues

--- contracts/UsdcCrowdfund.sol
+++ contracts/UsdcCrowdfund.sol
@@ -66,7 +66,7 @@ contract usdcCrowdfund {
         uint256 _target,
         uint256 _starttime,
         uint256 _endtime
-    ) public returns (bool) {
+    ) public {
         uint256 starting = block.timestamp + (_starttime * 1 minutes);
         uint256 ending = starting + (_endtime * 1 minutes);
         uint256 target = _target * (10**6);
@@ -89,7 +89,6 @@ contract usdcCrowdfund {
             Raised: 0,
             Withdrawn: false
         });
-        return true;
     }
 
     // function to pledge to a prticular campaign.
--- test/usdcTest.js
+++ test/usdcTest.js
@@ -1,8 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
-describe("UsdcCrowdfund contract", function(){
+
+
+describe("UsdcCrowdfund contract", async function(){
     // global vars
     let CrowdfundContract;
     let owner;
@@ -10,13 +13,17 @@ describe("UsdcCrowdfund contract", function(){
     let addr2;
     let usdcContract = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
     let UsdcCrowdfund;
+    let usdcCont;
+    let usdcToken;
 
     this.beforeEach(async function () {
         // Getting the contract factory and signers
         CrowdfundContract = await ethers.getContractFactory("usdcCrowdfund");
+        usdcCont = await ethers.getContractFactory("USDC");
         [owner, addr1, addr2] = await hre.ethers.getSigners();
 
         UsdcCrowdfund = await CrowdfundContract.deploy(usdcContract);
+        // usdcToken = await usdcCont.getContractAt();
     });
 
     describe("Deployment", function() {
@@ -31,20 +38,37 @@ describe("UsdcCrowdfund contract", function(){
     });
 
     describe("Functions", function() {
-        it("should list a campaign", async function() {
-            let id;
-            let title = "school fees payment";
-            let purpose = "please help";
+            let title = "school fees paymentss";
+            let purpose = "please help oo";
             let target = 20;
-            let startTime = 4;
-            let endTime = 10;
+            let startTime = 1;
+            let endTime = 200;
+
+        this.beforeEach(async function () {
+            
             await UsdcCrowdfund.listCampaign(title,purpose,target,startTime,endTime);
+        });
+
+        it("should list a campaign", async function() {
+
             const campaign = await UsdcCrowdfund.campaigns(1);
             
-            expect(campaign.CampaignNo).to.equal("1");
-            expect(campaign.Title).to.equal(title);
-            console.log(campaign.CampaignNo);
+            expect(campaign.CampaignNo).to.equal(1);
+            expect(campaign.Title).to.equal(title);   
+           
+            
+        });
 
+
+
+        it("should return all campaigns", async function() {
+            let amount = 20;
+            await helpers.time.increase(60);
+            await usdcToken.approve(UsdcCrowdfund.address(), amount)
+            const campaign = await UsdcCrowdfund.campaigns(1);
+            await UsdcCrowdfund.pledge(campaign.CampaignNo, amount);
+            expect(campaign.Raised).to.equal(amount);
+        
         });
 
 
